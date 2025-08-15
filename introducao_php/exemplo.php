40a5cc9e696b9c1a10ecab46b0df78e0c48e2e22
Victor <victorhvercosag@gmail.com>
2025-08-14 22:12:34 -0300

    é isso

--- introducao_php/exemplo.php
+++ introducao_php/exemplo.php
@@ -13,7 +13,20 @@
     echo "<p>  $dia  </p>";
 
     ?>
+
     <h1> Hoje é dia <?= $dia ?> de Agosto de 2025</h1>
+        <form method="post" action="FormularioResp.php">
+            <div class="mb-3">
+              <label for="nome" class="form-label">Informe o seu nome:</label>
+              <input type="text" id="nome" name="nome" class="form-control">
+            </div>
+
+            <div class="mb-3">
+              <label for="nome" class="form-label">Informe o seu nome:</label>
+              <input type="text" id="nome" name="nome" class="form-control">
+            </div>
+     <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
     
 </body>
 </html>
