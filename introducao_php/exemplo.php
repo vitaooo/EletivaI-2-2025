39723799d17f4a522fae0b1cb57415ee03662300
Victor <victorhvercosag@gmail.com>
2025-08-14 21:15:23 -0300

    introdução ao php

---
+++ introducao_php/exemplo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP zão</title>
+</head>
+<body>
+    <?php 
+    // date(d)
+    $dia = date("d");
+
+    echo "<p>  $dia  </p>";
+
+    ?>
+    <h1> Hoje é dia <?= $dia ?> de Agosto de 2025</h1>
+    
+</body>
+</html>
