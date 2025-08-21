a2001a75a1614189727061f1e4e10761e20da136
Victor <victorhvercosag@gmail.com>
2025-08-21 20:07:16 -0300

    criando o method post

---
+++ 21-08/exemplo1.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP zão</title>
+</head>
+<body>
+    <?php 
+    // date(d)
+    $dia = date("d");
+
+    echo "<p>  $dia  </p>";
+
+    ?>
+
+    <h1> Hoje é dia <?= $dia ?> de Agosto de 2025</h1>
+        <form method="post" >
+            <div class="mb-3">
+              <label for="numberx" class="form-label">Informe o valor de x:</label>
+              <input type="number" id="numberx" name="numberx" class="form-control">
+            </div>
+
+            <div class="mb-3">
+              <label for="numbery" class="form-label">Informe o valor de y:</label>
+              <input type="number" id="numbery" name="numbery" class="form-control">
+            </div>
+
+     <button type="submit" class="btn btn-primary">Enviar</button>
+
+        </form>
+
+<?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $x = $_POST['numberx'];
+    $y = $_POST['numbery'];
+    $soma = $x + $y;
+    echo "<p> A soma de $x e $y é $soma </p>";
+    }
+
+    // - + * **elevado
+?>
+    
+</body>
+</html>
---
+++ 21-08/resposta_exemplo1.php
@@ -0,0 +1,5 @@
+<?php
+    $x = $_POST['numberx'];
+    $y = $_POST['numbery'];
+    $soma = $x + $y;
+    echo "<p>A soma de $x + $y é $soma</p>";
